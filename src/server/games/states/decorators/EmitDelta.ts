2065035364b6e974aeb2136c47313218db2ae63b
iminlikewithyou <christian@omg.games>
2023-07-11 22:23:02 -0500

    chore: move all emit logic to one file

--- src/server/games/states/decorators/EmitDelta.ts
+++ src/server/games/states/decorators/EmitDelta.ts
@@ -1,6 +1,47 @@
 import "reflect-metadata";
 
-const skipDecorators = ["NoEmit", "EmitDelta"];
+// TODO a lot of repeated code here
+const skipDecorators = ["NoEmit", "EmitDelta", "EmitState"];
+
+function autoEmitDelta(originalMethod: Function, propertyKey: string) {
+  return function(...args: any[]) {
+    // "this" may be the wrong reference here
+    const result = originalMethod.apply(this, args);
+
+    this.emitDelta({
+      type: propertyKey,
+      payload: args
+    });
+
+    return result;
+  };
+}
+
+function autoEmitState(originalMethod: Function) {
+  return function(...args: any[]) {
+    const result = originalMethod.apply(this, args);
+
+    this.emitState(this.state);
+
+    return result;
+  };
+}
+
+export function EmitDelta(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+
+  Reflect.defineMetadata('EmitDelta', true, target, propertyKey);
+
+  descriptor.value = autoEmitDelta(originalMethod, propertyKey).bind(this);
+}
+
+export function EmitState(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+
+  Reflect.defineMetadata('EmitState', true, target, propertyKey);
+
+  descriptor.value = autoEmitState(originalMethod).bind(this);
+}
 
 export function AutoEmitDelta(constructor: Function) {
   // Get the prototype of the class
@@ -18,27 +59,30 @@ export function AutoEmitDelta(constructor: Function) {
     }
 
     const originalMethod = prototype[method];
-    prototype[method] = autoEmitDelta(originalMethod).bind(this); // binding this may be wrong
+    prototype[method] = autoEmitDelta(originalMethod, method).bind(this); // binding this may be wrong
   });
 }
 
-export function EmitDelta(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-  const originalMethod = descriptor.value;
+export function AutoEmitState(constructor: Function) {
+  // Get the prototype of the class
+  const prototype = constructor.prototype;
 
-  descriptor.value = autoEmitDelta(originalMethod).bind(this);
+  // Get all the method names of the class
+  const methods = Object.getOwnPropertyNames(prototype)
+    .filter(prop => typeof prototype[prop] === "function" && prop !== "constructor");
+
+  // Apply EmitDelta to each method, unless NoEmit is present
+  methods.forEach(method => {
+    if (skipDecorators.some(decorator => Reflect.getMetadata(decorator, prototype, method))) {
+      // Skip this method if the NoEmit decorator is present
+      return;
+    }
+
+    const originalMethod = prototype[method];
+    prototype[method] = autoEmitState(originalMethod).bind(this); // binding this may be wrong
+  });
 }
 
-function autoEmitDelta(originalMethod) {
-  const autoEmitter = (...args: any[]) => {
-    // "this" may be the wrong reference here
-    const result = originalMethod.apply(this, args);
-    const delta: any = {
-      type: "emitDelta",
-      payload: args
-    };
-    this.emitDelta(delta);
-    return result;
-  };
-
-  return autoEmitter;
+export function NoEmit(target: any, propertyKey: string, descriptor: PropertyDescriptor): PropertyDescriptor {
+  return descriptor;
 }
--- src/server/games/states/decorators/NoEmit.ts
+++
@@ -1,5 +0,0 @@
-function NoEmit(target: any, propertyKey: string, descriptor: PropertyDescriptor): PropertyDescriptor {
-  return descriptor;
-}
-
-export default NoEmit;
