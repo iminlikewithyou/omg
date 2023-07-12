1858ec2690145b3d34b81ce684b39a5d62bee4f7
iminlikewithyou <christian@omg.games>
2023-07-11 19:44:54 -0500

    chore: create a template GameState

---
+++ src/shared/games/states/GameState.ts
@@ -0,0 +1,50 @@
+// type OmitMethods<T, K extends keyof T> = Omit<T, K>;
+
+type FunctionParamsAndName<T, K extends keyof T> = T[K] extends (...args: any[]) => any ? {type: K, payload: Parameters<T[K]>} : never;
+
+export type DeltaUpdate<T> = { [K in keyof T]: FunctionParamsAndName<T, K> }[keyof T];
+
+// type BaseGameStateOmitted = OmitMethods<GameState, 'resetGame'>;
+type BaseDeltaUpdate = DeltaUpdate<GameState>;
+
+export class GameState {
+  // players: Player[] = [];
+
+  resetGame() {
+
+  }
+
+  movePlayer(id: string, newPosition: { x: number, y: number }) {
+    // const player = this.players.find(p => p.id === id);
+    // if (player) {
+    //   player.position = newPosition;
+    // }
+
+    let delta: BaseDeltaUpdate = {
+      type: "movePlayer",
+      payload: [id, newPosition]
+    };
+  }
+
+  // ...more methods...
+}
+
+type ChessDeltaUpdate = DeltaUpdate<ChessGameState>;
+
+export class ChessGameState extends GameState {
+  firstTurn: boolean = true;
+
+  movePiece(id: string, newPosition: { x: number, y: number }) {
+    // const piece = this.pieces.find(p => p.id === id);
+    // if (piece) {
+    //   piece.position = newPosition;
+    // }
+
+    let delta: ChessDeltaUpdate = {
+      type: "movePiece",
+      payload: [id, newPosition]
+    };
+  }
+
+  // ...more methods...
+}
