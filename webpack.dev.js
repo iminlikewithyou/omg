3c4c953842f7649c0ff58b55f57349672cc8e1fe
iminlikewithyou <christian@omg.games>
2023-12-30 12:20:29 -0600

    improve dev error handling

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,10 +3,11 @@ const common = require('./webpack.common.js');
 
 const LiveReloadPlugin = require("webpack-livereload-plugin");
 const OpenBrowserPlugin = require("open-browser-plugin");
+const path = require('path');
 
 module.exports = merge(common, {
   mode: "development",
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   plugins: [
     new LiveReloadPlugin({
         port: 9000,
