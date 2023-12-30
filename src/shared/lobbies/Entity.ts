3c900869658c6b8cd0e8026ad872566917a28576
iminlikewithyou <christian@omg.games>
2023-12-30 12:29:43 -0600

    entity extends NetworkObject

--- src/shared/lobbies/Entity.ts
+++ src/shared/lobbies/Entity.ts
@@ -1,9 +1,10 @@
+import { NetworkObject } from "../network/NetworkObject";
 import { BasePlayer } from "./BasePlayer";
 
 // might need a is(Entity) method in Entity
 // to test if a Player is part of a Team
 
-export interface Entity {
+export interface Entity extends NetworkObject {
   name: string;
   color: number;
 
